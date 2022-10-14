e9bfa87c91b9df43b1c79874490731e497e55db8
toandang <toandang2000ab@gmail.com>
2022-10-14 11:12:20 +0700

    commit

---
+++ src/main/java/com/toandd/project/thanhdat/source/boundary/TestController.java
@@ -0,0 +1,14 @@
+package com.toandd.project.thanhdat.source.boundary;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+    @GetMapping
+    public String getString() {
+        return "hello every one";
+    }
+}
